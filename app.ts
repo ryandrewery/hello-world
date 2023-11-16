377e4ab5edce5df147d6d5f51810e1f241cca89c
Don Syme <dsyme@users.noreply.github.com>
2023-11-16 14:45:48 +0000

    Update app.ts

--- app.ts
+++ app.ts
@@ -4,3 +4,5 @@ console.log("hello world");
 console.log("nice to meet you!");
 console.log("hello world");
 console.log("nice to meet you!");
+console.log("hello world");
+console.log("nice to meet you!");
